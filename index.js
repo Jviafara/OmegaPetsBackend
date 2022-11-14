026c739a7bd04124ff4c0e347854da56bd3ee4be
Jesus Viafara <jealvito@gmail.com>
2022-11-14 16:07:38 -0500

    Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('./app');
 
 var mongoose = require('./conexDB/conn.js');
 
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 	console.log('Servidor corriendo 0K!', port);
